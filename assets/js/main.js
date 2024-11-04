f4337d84320422c8ad03a8af1b132e0eaf65306d
Dinesh-kumar-002 <dkdon812002@gmail.com>
2024-11-04 16:26:58 +0530

    untill branding

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Wait for the page to load completely
   window.addEventListener("load", function() {
-    // Hide the preloader
     const preloader = document.getElementById('preloader');
     preloader.style.display = 'none';
-    
-    // Show the website content
-    // const content = document.querySelector('.content');
-    // content.style.display = 'block';
+ 
   });
 });
 
 
 
-// header bg to white
-var logo = document.querySelectorAll('.navbar-brand img')[0];
-var links = document.querySelectorAll('.nav-link');
-console.log(logo);
-
-window.addEventListener('scroll', function() {
-    const header = document.getElementById('navbar');
-    const bar = document.querySelector('.fa-bars');
-    
-    if (window.scrollY > 0) { // Adjust this value based on when you want the color change
-      header.classList.add('scrolled');
-      bar.style.color='#0c1b31';
-      logo.src="assets/images/logo/Logo-blue.png";
-      links.forEach(link => {
-        link.style.color="#071932";
-      });
-      
-    } else {
-      links.forEach(link => {
-        link.style.color="white";
-      });
-      logo.src="assets/images/logo/Logo-gold.png";
-      header.classList.remove('scrolled');
-      bar.style.color='white';
-    }
-});
-
-// header bg to white
 
 
 
@@ -60,3 +27,36 @@ document.querySelectorAll('#customer input').forEach(input => {
   });
   
 // input 
+
+
+
+const toggleBtn = document.querySelector(".navbar-toggler");
+const navbarNav = document.querySelector(".navbar-nav");
+const navCloseBtn = document.querySelector(".btn-nav-close");
+
+toggleBtn.addEventListener("click", () => {
+	navbarNav.classList.toggle("active");
+});
+navCloseBtn.addEventListener("click", () => {
+	navbarNav.classList.remove("active");
+});
+
+
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach((item) => {
+	const hasDropdowns = item.querySelector(".dropdown") !== null;
+	if (hasDropdowns) {
+		item.classList.add("icon");
+	}
+});
+
+
+$(document).ready(()=>{
+  var floatingBannerImage = $('.floating-banner-image');
+  floatingBannerImage.addClass('anime');
+})
+
+
+
+
